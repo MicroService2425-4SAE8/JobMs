8c38e9142992f1c881165969d73c8f0af5588704
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-07 08:29:13 +0100

    first commit

---
+++ src/main/java/com/job/jobsae8/JobSae8Application.java
@@ -0,0 +1,25 @@
+package com.job.jobsae8;
+
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class JobSae8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobSae8Application.class, args);
+    }
+    @Bean
+    ApplicationRunner init(JobRepository jobRepository) {
+        return (args) -> {
+            // save
+            jobRepository.save(new Job("Service Financier", true));
+            jobRepository.save(new Job("Service Info", false));
+            jobRepository.save(new Job("Service RH", false));
+            // fetch
+            jobRepository.findAll().forEach(System.out::println);
+        };
+    }
+}
---
+++ src/test/java/com/job/jobsae8/JobSae8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.job.jobsae8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSae8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
