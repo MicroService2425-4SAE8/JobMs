8434aa95f0588480bf87ab7fee0fc859566c8e35
Oumeima-IbnElfekih <oumeima.ibnelfekih@esprit.tn>
2025-02-14 09:18:55 +0100

    add eureka config

--- src/main/java/com/job/jobsae8/JobSae8Application.java
+++ src/main/java/com/job/jobsae8/JobSae8Application.java
@@ -3,9 +3,11 @@ package com.job.jobsae8;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class JobSae8Application {
 
     public static void main(String[] args) {
